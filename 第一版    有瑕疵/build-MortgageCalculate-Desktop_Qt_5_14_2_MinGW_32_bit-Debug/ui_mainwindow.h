9c418c1aa42689656a853d5899388118fdc53f47
C-S-Cong <56379637+Ledger-cs@users.noreply.github.com>
2020-05-23 21:37:38 +0800

    first shujupiancha

---
+++ 第一版    有瑕疵/build-MortgageCalculate-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,279 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextBrowser>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QPushButton *pushButton;
+    QPlainTextEdit *plainTextEdit;
+    QPlainTextEdit *plainTextEdit_2;
+    QComboBox *comboBox;
+    QComboBox *comboBox_2;
+    QPlainTextEdit *plainTextEdit_3;
+    QLineEdit *lineEdit;
+    QLineEdit *lineEdit_2;
+    QPlainTextEdit *plainTextEdit_4;
+    QPlainTextEdit *plainTextEdit_5;
+    QLineEdit *lineEdit_3;
+    QLineEdit *lineEdit_4;
+    QPlainTextEdit *plainTextEdit_6;
+    QPlainTextEdit *plainTextEdit_7;
+    QComboBox *comboBox_3;
+    QComboBox *comboBox_4;
+    QPlainTextEdit *plainTextEdit_8;
+    QLineEdit *lineEdit_5;
+    QLineEdit *lineEdit_6;
+    QPlainTextEdit *plainTextEdit_9;
+    QPlainTextEdit *plainTextEdit_10;
+    QComboBox *comboBox_5;
+    QTextBrowser *textBrowser;
+    QPlainTextEdit *plainTextEdit_11;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(705, 572);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(340, 10, 111, 41));
+        plainTextEdit = new QPlainTextEdit(centralwidget);
+        plainTextEdit->setObjectName(QString::fromUtf8("plainTextEdit"));
+        plainTextEdit->setGeometry(QRect(10, 10, 91, 31));
+        plainTextEdit_2 = new QPlainTextEdit(centralwidget);
+        plainTextEdit_2->setObjectName(QString::fromUtf8("plainTextEdit_2"));
+        plainTextEdit_2->setGeometry(QRect(10, 50, 91, 31));
+        comboBox = new QComboBox(centralwidget);
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+        comboBox->setGeometry(QRect(130, 10, 171, 31));
+        comboBox_2 = new QComboBox(centralwidget);
+        comboBox_2->addItem(QString());
+        comboBox_2->addItem(QString());
+        comboBox_2->setObjectName(QString::fromUtf8("comboBox_2"));
+        comboBox_2->setGeometry(QRect(130, 50, 171, 31));
+        plainTextEdit_3 = new QPlainTextEdit(centralwidget);
+        plainTextEdit_3->setObjectName(QString::fromUtf8("plainTextEdit_3"));
+        plainTextEdit_3->setGeometry(QRect(10, 90, 91, 87));
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+        lineEdit->setGeometry(QRect(130, 90, 81, 31));
+        lineEdit_2 = new QLineEdit(centralwidget);
+        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+        lineEdit_2->setGeometry(QRect(130, 140, 81, 31));
+        plainTextEdit_4 = new QPlainTextEdit(centralwidget);
+        plainTextEdit_4->setObjectName(QString::fromUtf8("plainTextEdit_4"));
+        plainTextEdit_4->setGeometry(QRect(220, 90, 81, 81));
+        plainTextEdit_5 = new QPlainTextEdit(centralwidget);
+        plainTextEdit_5->setObjectName(QString::fromUtf8("plainTextEdit_5"));
+        plainTextEdit_5->setGeometry(QRect(10, 190, 91, 87));
+        lineEdit_3 = new QLineEdit(centralwidget);
+        lineEdit_3->setObjectName(QString::fromUtf8("lineEdit_3"));
+        lineEdit_3->setGeometry(QRect(130, 190, 81, 31));
+        lineEdit_4 = new QLineEdit(centralwidget);
+        lineEdit_4->setObjectName(QString::fromUtf8("lineEdit_4"));
+        lineEdit_4->setGeometry(QRect(130, 240, 81, 31));
+        plainTextEdit_6 = new QPlainTextEdit(centralwidget);
+        plainTextEdit_6->setObjectName(QString::fromUtf8("plainTextEdit_6"));
+        plainTextEdit_6->setGeometry(QRect(220, 190, 81, 81));
+        plainTextEdit_7 = new QPlainTextEdit(centralwidget);
+        plainTextEdit_7->setObjectName(QString::fromUtf8("plainTextEdit_7"));
+        plainTextEdit_7->setGeometry(QRect(10, 290, 91, 81));
+        comboBox_3 = new QComboBox(centralwidget);
+        comboBox_3->addItem(QString());
+        comboBox_3->addItem(QString());
+        comboBox_3->addItem(QString());
+        comboBox_3->addItem(QString());
+        comboBox_3->addItem(QString());
+        comboBox_3->addItem(QString());
+        comboBox_3->addItem(QString());
+        comboBox_3->setObjectName(QString::fromUtf8("comboBox_3"));
+        comboBox_3->setGeometry(QRect(130, 290, 87, 31));
+        comboBox_4 = new QComboBox(centralwidget);
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->addItem(QString());
+        comboBox_4->setObjectName(QString::fromUtf8("comboBox_4"));
+        comboBox_4->setGeometry(QRect(130, 340, 87, 31));
+        plainTextEdit_8 = new QPlainTextEdit(centralwidget);
+        plainTextEdit_8->setObjectName(QString::fromUtf8("plainTextEdit_8"));
+        plainTextEdit_8->setGeometry(QRect(10, 380, 91, 81));
+        lineEdit_5 = new QLineEdit(centralwidget);
+        lineEdit_5->setObjectName(QString::fromUtf8("lineEdit_5"));
+        lineEdit_5->setGeometry(QRect(130, 380, 91, 31));
+        lineEdit_6 = new QLineEdit(centralwidget);
+        lineEdit_6->setObjectName(QString::fromUtf8("lineEdit_6"));
+        lineEdit_6->setGeometry(QRect(130, 430, 91, 31));
+        plainTextEdit_9 = new QPlainTextEdit(centralwidget);
+        plainTextEdit_9->setObjectName(QString::fromUtf8("plainTextEdit_9"));
+        plainTextEdit_9->setGeometry(QRect(230, 380, 71, 81));
+        plainTextEdit_10 = new QPlainTextEdit(centralwidget);
+        plainTextEdit_10->setObjectName(QString::fromUtf8("plainTextEdit_10"));
+        plainTextEdit_10->setGeometry(QRect(10, 470, 91, 31));
+        comboBox_5 = new QComboBox(centralwidget);
+        comboBox_5->addItem(QString());
+        comboBox_5->addItem(QString());
+        comboBox_5->setObjectName(QString::fromUtf8("comboBox_5"));
+        comboBox_5->setGeometry(QRect(130, 470, 87, 31));
+        textBrowser = new QTextBrowser(centralwidget);
+        textBrowser->setObjectName(QString::fromUtf8("textBrowser"));
+        textBrowser->setGeometry(QRect(340, 100, 321, 411));
+        plainTextEdit_11 = new QPlainTextEdit(centralwidget);
+        plainTextEdit_11->setObjectName(QString::fromUtf8("plainTextEdit_11"));
+        plainTextEdit_11->setGeometry(QRect(340, 60, 321, 31));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 705, 26));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "\345\274\200\345\247\213\350\256\241\347\256\227", nullptr));
+        plainTextEdit->setPlainText(QCoreApplication::translate("MainWindow", "\350\264\267\346\254\276\347\261\273\345\210\253", nullptr));
+        plainTextEdit_2->setPlainText(QCoreApplication::translate("MainWindow", "\350\264\267\346\254\276\346\226\271\345\274\217", nullptr));
+        comboBox->setItemText(0, QCoreApplication::translate("MainWindow", "\345\225\206\344\270\232\350\264\267\346\254\276", nullptr));
+        comboBox->setItemText(1, QCoreApplication::translate("MainWindow", "\345\205\254\347\247\257\351\207\221\350\264\267\346\254\276", nullptr));
+        comboBox->setItemText(2, QCoreApplication::translate("MainWindow", "\347\273\204\345\220\210\345\236\213\350\264\267\346\254\276", nullptr));
+
+        comboBox_2->setItemText(0, QCoreApplication::translate("MainWindow", "\346\240\271\346\215\256\351\235\242\347\247\257\343\200\201\345\215\225\344\273\267\350\256\241\347\256\227", nullptr));
+        comboBox_2->setItemText(1, QCoreApplication::translate("MainWindow", "\346\240\271\346\215\256\350\264\267\346\254\276\346\200\273\351\242\235\350\256\241\347\256\227", nullptr));
+
+        plainTextEdit_3->setPlainText(QCoreApplication::translate("MainWindow", "\346\210\277\345\261\213\345\215\225\344\273\267\n"
+"\n"
+"\n"
+"\346\210\277\345\261\213\351\235\242\347\247\257", nullptr));
+        plainTextEdit_4->setPlainText(QCoreApplication::translate("MainWindow", "\345\205\203/\345\271\263\347\261\263\n"
+"\n"
+"\n"
+"\345\271\263\347\261\263", nullptr));
+        plainTextEdit_5->setPlainText(QCoreApplication::translate("MainWindow", "\345\225\206\350\264\267\350\264\267\346\254\276\n"
+"\n"
+"\n"
+"\345\205\254\347\247\257\351\207\221\350\264\267\346\254\276", nullptr));
+        plainTextEdit_6->setPlainText(QCoreApplication::translate("MainWindow", "\344\270\207\345\205\203\n"
+"\n"
+"\n"
+"\344\270\207\345\205\203", nullptr));
+        plainTextEdit_7->setPlainText(QCoreApplication::translate("MainWindow", "\346\214\211\346\217\255\346\210\220\346\225\260\n"
+"\n"
+"\n"
+"\346\214\211\346\217\255\345\271\264\346\225\260", nullptr));
+        comboBox_3->setItemText(0, QCoreApplication::translate("MainWindow", "9\346\210\220", nullptr));
+        comboBox_3->setItemText(1, QCoreApplication::translate("MainWindow", "8\346\210\220", nullptr));
+        comboBox_3->setItemText(2, QCoreApplication::translate("MainWindow", "7\346\210\220", nullptr));
+        comboBox_3->setItemText(3, QCoreApplication::translate("MainWindow", "6\346\210\220", nullptr));
+        comboBox_3->setItemText(4, QCoreApplication::translate("MainWindow", "5\346\210\220", nullptr));
+        comboBox_3->setItemText(5, QCoreApplication::translate("MainWindow", "4\346\210\220", nullptr));
+        comboBox_3->setItemText(6, QCoreApplication::translate("MainWindow", "3\346\210\220", nullptr));
+
+        comboBox_4->setItemText(0, QCoreApplication::translate("MainWindow", "1\345\271\264", nullptr));
+        comboBox_4->setItemText(1, QCoreApplication::translate("MainWindow", "2\345\271\264", nullptr));
+        comboBox_4->setItemText(2, QCoreApplication::translate("MainWindow", "3\345\271\264", nullptr));
+        comboBox_4->setItemText(3, QCoreApplication::translate("MainWindow", "4\345\271\264", nullptr));
+        comboBox_4->setItemText(4, QCoreApplication::translate("MainWindow", "5\345\271\264", nullptr));
+        comboBox_4->setItemText(5, QCoreApplication::translate("MainWindow", "6\345\271\264", nullptr));
+        comboBox_4->setItemText(6, QCoreApplication::translate("MainWindow", "7\345\271\264", nullptr));
+        comboBox_4->setItemText(7, QCoreApplication::translate("MainWindow", "8\345\271\264", nullptr));
+        comboBox_4->setItemText(8, QCoreApplication::translate("MainWindow", "9\345\271\264", nullptr));
+        comboBox_4->setItemText(9, QCoreApplication::translate("MainWindow", "10\345\271\264", nullptr));
+        comboBox_4->setItemText(10, QCoreApplication::translate("MainWindow", "11\345\271\264", nullptr));
+        comboBox_4->setItemText(11, QCoreApplication::translate("MainWindow", "12\345\271\264", nullptr));
+        comboBox_4->setItemText(12, QCoreApplication::translate("MainWindow", "13\345\271\264", nullptr));
+        comboBox_4->setItemText(13, QCoreApplication::translate("MainWindow", "14\345\271\264", nullptr));
+        comboBox_4->setItemText(14, QCoreApplication::translate("MainWindow", "15\345\271\264", nullptr));
+        comboBox_4->setItemText(15, QCoreApplication::translate("MainWindow", "16\345\271\264", nullptr));
+        comboBox_4->setItemText(16, QCoreApplication::translate("MainWindow", "17\345\271\264", nullptr));
+        comboBox_4->setItemText(17, QCoreApplication::translate("MainWindow", "18\345\271\264", nullptr));
+        comboBox_4->setItemText(18, QCoreApplication::translate("MainWindow", "19\345\271\264", nullptr));
+        comboBox_4->setItemText(19, QCoreApplication::translate("MainWindow", "20\345\271\264", nullptr));
+        comboBox_4->setItemText(20, QCoreApplication::translate("MainWindow", "21\345\271\264", nullptr));
+        comboBox_4->setItemText(21, QCoreApplication::translate("MainWindow", "22\345\271\264", nullptr));
+        comboBox_4->setItemText(22, QCoreApplication::translate("MainWindow", "23\345\271\264", nullptr));
+        comboBox_4->setItemText(23, QCoreApplication::translate("MainWindow", "24\345\271\264", nullptr));
+        comboBox_4->setItemText(24, QCoreApplication::translate("MainWindow", "25\345\271\264", nullptr));
+
+        plainTextEdit_8->setPlainText(QCoreApplication::translate("MainWindow", "\345\225\206\350\264\267\345\210\251\347\216\207\n"
+"\n"
+"\n"
+"\345\205\254\347\247\257\351\207\221\345\210\251\347\216\207", nullptr));
+        plainTextEdit_9->setPlainText(QCoreApplication::translate("MainWindow", "%\n"
+"\n"
+"\n"
+"%", nullptr));
+        plainTextEdit_10->setPlainText(QCoreApplication::translate("MainWindow", "\350\277\230\346\254\276\346\226\271\345\274\217", nullptr));
+        comboBox_5->setItemText(0, QCoreApplication::translate("MainWindow", "\347\255\211\351\242\235\346\234\254\346\201\257", nullptr));
+        comboBox_5->setItemText(1, QCoreApplication::translate("MainWindow", "\347\255\211\351\242\235\346\234\254\351\207\221", nullptr));
+
+        plainTextEdit_11->setPlainText(QCoreApplication::translate("MainWindow", "\350\256\241\347\256\227\347\273\223\346\236\234", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
